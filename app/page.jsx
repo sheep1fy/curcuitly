b52c987cd3e1e50410106c20f2bc05002e65621b
Sheepify <186878545+sheep1fy@users.noreply.github.com>
2026-06-19 10:57:26 +1000

    Delete app directory

--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 style={{ color: "white", background: "black", padding: 40 }}>
-        CURCUITLY ROUTE TEST
-      </h1>
-    </main>
-  );
-}
